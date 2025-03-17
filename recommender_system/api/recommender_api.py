5fd1182af7ac9ba10b9b6126e786b0fa69fad38c
Maria Ait_Ferhat <km_aitferhat@esi.dz>
2025-03-17 09:36:13 +0100

    Implementing an endpoint fastapi to get the trending movies

--- recommender_system/api/recommender_api.py
+++ recommender_system/api/recommender_api.py
@@ -1,1 +1,29 @@
-# recommender_api.py
+from fastapi import FastAPI
+import pandas as pd
+import os
+
+app = FastAPI()  # Initialize the FastAPI app
+
+BASE_DIR = os.path.join('..')  # Navigate up one level to recommender_system
+filepath = os.path.join(BASE_DIR, 'data', 'database_data', 'trending_movies.csv')
+
+def load_trending_movies_from_csv():
+    
+    try:
+        trending_movies = pd.read_csv(filepath,  nrows=100)
+        return trending_movies
+    except FileNotFoundError:
+        print(f"File not found: {filepath}")
+        return None
+    except Exception as e:
+        print(f"Error loading trending movies from {filepath}: {e}")
+        return None
+
+@app.get('/api/trending_movies')
+def get_trending_movies():
+    trending_movies = load_trending_movies_from_csv()
+    if trending_movies is not None:
+        movies_json = trending_movies.to_dict(orient='records')
+        return movies_json
+    else:
+        return {"error": "Trending movies data not available"}
