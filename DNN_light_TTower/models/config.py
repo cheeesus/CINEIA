6680db4c4e9e99cf0a2c0a102f3eac27eb3ea8b4
AaronLin <68621077+Epochex@users.noreply.github.com>
2025-05-05 22:07:11 +0200

    delete useless Model Structuer folders

--- DNN_light_TTower/models/config.py
+++
@@ -1,6 +0,0 @@
-# models/config.py
-DB_NAME = "rospars_yann"
-DB_USER = "rospars_01"
-DB_PASSWORD = "Projet1234"
-DB_HOST = "postgresql-rospars.alwaysdata.net"
-DB_PORT = 5432
--- TraditionML_Heavy/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-DB_NAME = "rospars_yann"
-DB_USER = "rospars_01"
-DB_PASSWORD = "Projet1234"
-DB_HOST = "postgresql-rospars.alwaysdata.net"
-DB_PORT = 5432
